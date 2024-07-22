6e3887cab37736ede5a133cc1f84fcae8cdf8810
Konstantin <scub111g@gmail.com>
2024-07-22 18:37:47 +0300

    fix: set default value

--- deploy/constants.ts
+++ deploy/constants.ts
@@ -4,5 +4,5 @@ export const SOURCE_NUMBER_DELIMITER = '.';
 export const TARGET_NUMBER_DELIMITER = ',';
 
 export const DEPOSIT_CONTRACT_ADDRESS = '0xEf094C9c1c767b31d33a982E8DB0eeDE9564Af63';
-export const BYPASS_CONTRACT_CHECK = true;
+export const BYPASS_CONTRACT_CHECK = false;
 export const VESTING_TOKEN_PRICE = 0.1234;
